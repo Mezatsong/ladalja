cbbd5ce4673ff459d5b8c88b435f035842fcd74c
Ben677 <meztsacar@gmail.com>
2018-09-04 12:54:17 +0100

    initial commit

---
+++ src/main/java/ben/ladalja/Ignore.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package ben.ladalja;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Fields that use this annotation will be ignored in the attributes list.<br>
+ * For example, if you have a something like that
+ * <p>
+ * <pre>
+ * public class Example {
+ * 		private Long id;
+ * 		private String firstName;
+ * 		private String lastName;
+ * 		<code>@Ignore</code> private String name;
+ * }
+ * </pre>
+ * The attributes list will be id,firstName and lastName and name field will be completely ignored<br> 
+ * when loading object from database or inserting object in database.
+ * 
+ * @author MEZATSONG TSAFACK Carrel, meztsacar@gmail.com
+ *
+ */
+@Documented
+@Retention(RUNTIME)
+@Target(FIELD)
+public @interface Ignore {
+}
---
+++ src/main/java/ben/ladalja/QueryListener.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ben.ladalja;
+
+/**
+ * This interface provide only one method for listening every SQL query through Ladalja.
+ * @author MEZATSONG TSAFACK Carrel, meztsacar@gmail.com
+ */
+public interface QueryListener {
+	
+	/**
+	 * This method will be called before each query execution
+	 * @param query which will be executed
+	 */
+	void listen(String query);
+}
